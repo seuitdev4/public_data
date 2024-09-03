e3ac3283637b9410c70c66e4369bc96ff133ce04
SEUitdev5 <it.dev5@seu.edu.sa>
2024-09-03 09:53:22 +0300

    Request Data Console

--- resources/views/public_data/index.blade.php
+++ resources/views/public_data/index.blade.php
@@ -346,6 +346,24 @@
 
             $('#outputContainer').text('Loading...');
 
+           
+    var serializedData = $(this).serializeArray();
+    var requestData = {};
+
+    // Convert serialized array to an object
+    $.each(serializedData, function() {
+        if (this.name in requestData) {
+            if (!Array.isArray(requestData[this.name])) {
+                requestData[this.name] = [requestData[this.name]];
+            }
+            requestData[this.name].push(this.value);
+        } else {
+            requestData[this.name] = this.value;
+        }
+    });
+
+    console.log('Request data:', requestData);
+
             $.ajax({
                 url: $(this).attr('action'),
                 type: 'GET',
